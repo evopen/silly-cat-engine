8a562e5983101085b617784e6ac9eee00f9b3f09
evopen <520dhh@gmail.com>
2021-02-06 21:06:44 +0800

    update coverage test

--- safe-vk/src/lib.rs
+++ safe-vk/src/lib.rs
@@ -1,7 +1,22 @@
 #[cfg(test)]
 mod tests {
+    use crate::fuck;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_fuck() {
+        fuck(true);
+    }
+}
+
+pub fn fuck(a: bool) {
+    if a {
+        println!("fucK");
+    } else {
+        println!("fucK you");
     }
 }
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,44 +1,63 @@
 fn main() {
-    std::process::Command::new("cargo")
+    std::fs::read_dir("target/debug/deps")
+        .unwrap()
+        .map(|p| p.unwrap().path())
+        .filter(|p| p.extension().is_some())
+        .filter(|p| {
+            let ext = p.extension().unwrap().to_str().unwrap();
+            ext.eq("gcda") || ext.eq("gcno")
+        })
+        .for_each(|p| std::fs::remove_file(p).unwrap());
+    let output = std::process::Command::new("cargo")
         .arg("build")
         .args(&["--package", "safe-vk"])
         .env("RUSTFLAGS", "-Zinstrument-coverage")
         .output()
-        .unwrap();
+        .unwrap()
+        .stderr;
+    println!("{}", std::str::from_utf8(&output).unwrap());
 
-    std::process::Command::new("cargo")
+    let output = std::process::Command::new("cargo")
         .arg("test")
         .args(&["--package", "safe-vk"])
         .env("RUSTFLAGS", "-Zinstrument-coverage")
         .output()
-        .unwrap();
+        .unwrap()
+        .stderr;
+    println!("{}", std::str::from_utf8(&output).unwrap());
 
-    std::process::Command::new("cargo")
+    let output = std::process::Command::new("cargo")
         .arg("build")
         .args(&["--package", "safe-vk"])
         .env("CARGO_INCREMENTAL", "0")
         .env("RUSTFLAGS", "-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort")
         .env("RUSTDOCFLAGS", "-Cpanic=abort")
         .output()
-        .unwrap();
+        .unwrap()
+        .stderr;
+    println!("{}", std::str::from_utf8(&output).unwrap());
 
-    std::process::Command::new("cargo")
+    let output = std::process::Command::new("cargo")
         .arg("test")
         .args(&["--package", "safe-vk"])
         .env("CARGO_INCREMENTAL", "0")
         .env("RUSTFLAGS", "-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort")
         .env("RUSTDOCFLAGS", "-Cpanic=abort")
         .output()
-        .unwrap();
+        .unwrap()
+        .stderr;
+    println!("{}", std::str::from_utf8(&output).unwrap());
 
-    std::process::Command::new("grcov")
-        .arg(".")
-        .args(&["-s", "."])
+    let output = std::process::Command::new("grcov")
+        .arg("safe-vk/default.profraw")
+        .args(&["-s", "./safe-vk"])
         .args(&["--binary-path", "./target/debug/"])
         .args(&["-t", "html"])
         .arg("--branch")
         .arg("--ignore-not-existing")
-        .args(&["-o", "./target/debug/coverage/"])
+        .args(&["-o", "target/debug/coverage/"])
         .output()
-        .unwrap();
+        .unwrap()
+        .stderr;
+    println!("{}", std::str::from_utf8(&output).unwrap());
 }

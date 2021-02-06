0df6d87659f8e36b578992728a93e7a2b6ea7297
evopen <520dhh@gmail.com>
2021-02-06 20:24:52 +0800

    coverage test report

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    std::process::Command::new("cargo")
+        .arg("build")
+        .args(&["--package", "safe-vk"])
+        .env("RUSTFLAGS", "-Zinstrument-coverage")
+        .output()
+        .unwrap();
+
+    std::process::Command::new("cargo")
+        .arg("test")
+        .args(&["--package", "safe-vk"])
+        .env("RUSTFLAGS", "-Zinstrument-coverage")
+        .output()
+        .unwrap();
+
+    std::process::Command::new("cargo")
+        .arg("build")
+        .args(&["--package", "safe-vk"])
+        .env("CARGO_INCREMENTAL", "0")
+        .env("RUSTFLAGS", "-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort")
+        .env("RUSTDOCFLAGS", "-Cpanic=abort")
+        .output()
+        .unwrap();
+
+    std::process::Command::new("cargo")
+        .arg("test")
+        .args(&["--package", "safe-vk"])
+        .env("CARGO_INCREMENTAL", "0")
+        .env("RUSTFLAGS", "-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort")
+        .env("RUSTDOCFLAGS", "-Cpanic=abort")
+        .output()
+        .unwrap();
+
+    std::process::Command::new("grcov")
+        .arg(".")
+        .args(&["-s", "."])
+        .args(&["--binary-path", "./target/debug/"])
+        .args(&["-t", "html"])
+        .arg("--branch")
+        .arg("--ignore-not-existing")
+        .args(&["-o", "./target/debug/coverage/"])
+        .output()
+        .unwrap();
 }

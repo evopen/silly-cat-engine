20b63f714b273d562798ba03bc7dbf5d861b17e9
evopen <520dhh@gmail.com>
2021-02-08 08:52:56 +0800

    fix xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -62,11 +62,12 @@ fn main() {
     println!("{}", std::str::from_utf8(&output).unwrap());
 
     let output = std::process::Command::new("grcov")
-        .arg("safe-vk/default.profraw")
+        .arg(".")
         .args(&["-s", "./safe-vk/src"])
         .args(&["--binary-path", "./target/coverage/source/debug/"])
         .args(&["-t", "html"])
         .arg("--branch")
+        .arg("--llvm")
         .arg("--ignore-not-existing")
         .args(&["-o", "target/coverage/report/"])
         .output()
